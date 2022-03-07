4d2f9b2437fa6bea562860fd8e124fc2c63b987b
diyanarmln <diyana.rmln@gmail.com>
2022-03-08 01:13:12 +0800

    configure seed data

---
+++ seeders/20220307170434-seed-data.js
@@ -0,0 +1,33 @@
+const jsSHA = require('jssha');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const userPassword = 'qwerty';
+    const shaObj = new jsSHA('SHA-512', 'TEXT', { encoding: 'UTF8' });
+    shaObj.update(userPassword);
+    const hashedPassword = shaObj.getHash('HEX');
+    
+    const userList = [
+
+      {
+        email: 'tom@gmail.com',
+        password: hashedPassword,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        email: 'jerry@gmail.com',
+        password: hashedPassword,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]
+
+    const users = await queryInterface.bulkInsert('users', userList, { returning: true });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
